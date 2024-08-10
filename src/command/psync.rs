c8913adb8b2f208ae705a92a16f1764f99dfadaf
Ludin Nento <ludyyn@gmail.com>
2024-08-11 02:16:59 +0800

    fix psync send command

--- src/command/psync.rs
+++ src/command/psync.rs
@@ -25,7 +25,7 @@ impl Psync {
                     let resp_value = Value::SimpleString(format!("FULLRESYNC {} 0", repl.master_replid));
                     conn.write_value(resp_value).await?;
 
-                    let rdb_str = "24544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+                    let rdb_str = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
                     let rdb = hex::decode(rdb_str);
 
                     match rdb {
